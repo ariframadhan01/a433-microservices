016a3385bed0de1282a6119e3a6f8ccaa5bb96aa
ariframadhan01 <aramadhan594@gmail.com>
2025-03-01 14:40:25 +0700

    Add script build push docker image

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Username Docker Hub
+DOCKER_HUB_USERNAME="ariframadhan"
+
+# Nama dan tag image
+IMAGE_NAME="karsajobs"
+IMAGE_TAG="latest"
+IMAGE_FULL_NAME="$DOCKER_HUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
+
+# 1. Build Docker image dari Dockerfile
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
+
+# 2. Melihat daftar image di lokal
+echo "Listing local Docker images..."
+docker images
+
+# 3. Mengubah nama image agar sesuai dengan format Docker Hub
+echo "Tagging image for Docker Hub..."
+docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_FULL_NAME
+
+# 4. Login ke Docker Hub
+echo "Logging into Docker Hub..."
+echo $PASSWORD_DOCKER_HUB | docker login -u $DOCKER_HUB_USERNAME --password-stdin
+
+# 5. Mengunggah image ke Docker Hub
+echo "Pushing image to Docker Hub..."
+docker push $IMAGE_FULL_NAME
+
+echo "Docker image successfully pushed to Docker Hub!"
